3206625a887723faf303fa91b6edc85caa8d1df8
Eli Brown <eliyellow@gmail.com>
2024-04-09 23:31:03 -0500

    adding changes

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,28 +15,37 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName(name) {
-  return;
+return 'Hello Eli', name;
 }
+console.log();
+ 
+
 // Remember to call the function to test
 
 
 // 3. Function to add two numbers together & return the result
-function addNumbers(firstNumber) {
-  // return firstNumber + secondNumber;
+function addNumbers(firstNumber, secondNumber) {
+  return firstNumber + secondNumber
 }
-
+  // return firstNumber + secondNumber;
+  addNumbers(4, 5)
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree() {
+function multiplyThree(x, y, z) {
+  return x * y * z;
 
 }
-
+let result = multiplyThree(2, 4, 6);
+console.log('result', result);
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
+let number = true;
 function isPositive(number) {
   if (number > 0) {
-    return;
+ 
+  console.log('number', number);
+  
   }
   return;
 }
